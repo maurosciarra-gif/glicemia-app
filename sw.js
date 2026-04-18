d3f28d67888b853578523230446b76ac7fb43f6b
maurosciarra-gif <mauro.sciarra@gmail.com>
2026-04-18 19:21:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'glicemia-app-v1';
+const CACHE_NAME = 'glicemia-app-v2';
 
 const urlsToCache = [
   './',
@@ -8,14 +8,35 @@ const urlsToCache = [
   './icon-512.png'
 ];
 
+// INSTALL
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// ACTIVATE
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// FETCH
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request).catch(() => caches.match('./index.html'));
+      })
   );
 });
